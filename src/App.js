eae5de32baee0441b0ea235983cc0c98e8868157
Leandro Ordoñez <leandro.o.ar@hotmail.com>
2024-03-15 18:27:24 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 function App() {
-  return <section className="h-full container"></section>;
+  return (
+    <section className="py-12 px-6 mt-0 mx-3 flex">
+      <div className="flex-1 xl:flex-initial xl:w-5/6">Hola</div>
+    </section>
+  );
 }
 
 export default App;
